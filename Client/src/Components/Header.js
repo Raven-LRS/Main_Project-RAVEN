9402ffe43bd6a2fdb499c2055704b3fc24bd4c2f
Venkat <dvsuniq22@gmail.com>
2023-03-28 12:47:23 +0530

    Split and Merge Design Finished

--- Client/src/Components/Header.js
+++ Client/src/Components/Header.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import "../App.css";
+
 export default function Header() {
+  const [data, setdata] = useState({
+    address: null,
+    Balance: null,
+  });
+  const [log, setlog] = useState("LOGIN");
+  const btnhandler = () => {
+    // Asking if metamask is already present or not
+    if (window.ethereum) {
+      // res[0] for fetching a first wallet
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((res) => accountChangeHandler(res[0]));
+      console.log("connected");
+    } else {
+      alert("install metamask extension!!");
+    }
+    setlog("LOGOUT");
+  };
+
+  const getbalance = (address) => {
+    // Requesting balance method
+    window.ethereum
+      .request({
+        method: "eth_getBalance",
+        params: [address, "latest"],
+      })
+      .then((balance) => {
+        // Setting balance
+        setdata({
+          Balance: ethers.utils.formatEther(balance),
+        });
+      });
+  };
+
+  // Function for getting handling all events
+  const accountChangeHandler = (account) => {
+    // Setting an address data
+    setdata({
+      address: account,
+    });
+
+    // Setting a balance
+    getbalance(account);
+  };
+
+  //-------
   return (
     <>
       <img
@@ -12,7 +61,9 @@ export default function Header() {
       <div className="head">
         <h1>RAVEN</h1>
       </div>
-      <button className="log">LOGIN</button>
+      <button onClick={btnhandler} className="log">
+        {log}
+      </button>
     </>
   );
 }
