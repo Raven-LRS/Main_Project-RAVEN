9cb4e43af7a5c06080b2c61bfabfc1ae254ef933
Venkat <dvsuniq22@gmail.com>
2023-05-15 10:57:50 +0530

    file path set

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 const corsOptions = {
   origin: "http://localhost:3000",
-  credentials: true, 
+  credentials: true,
   //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
@@ -20,7 +20,7 @@ app.post("/submit-form", (req, res) => {
   const formData = req.body;
 
   try {
-    fs.writeFileSync("data.json", JSON.stringify(formData));
+    fs.writeFileSync("C:/Users/Public/data.json", JSON.stringify(formData));
     res.json({ success: true });
   } catch (error) {
     console.error("Error:", error);
